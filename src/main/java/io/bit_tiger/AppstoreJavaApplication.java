2ff5571b0011e24bd94a562d38ac90c731c2193a
Zexi Jesse Zhuang <zhzexi@gmail.com>
2016-06-07 21:59:06 -0700

    initial commit with sprint boot and mysql

---
+++ src/main/java/io/bit_tiger/AppstoreJavaApplication.java
@@ -0,0 +1,14 @@
+package io.bit_tiger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppstoreJavaApplication {
+
+	public static void main(String[] args) {
+
+
+		SpringApplication.run(AppstoreJavaApplication.class, args);
+	}
+}
---
+++ src/test/java/io/bit_tiger/AppstoreJavaApplicationTests.java
@@ -0,0 +1,18 @@
+package io.bit_tiger;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = AppstoreJavaApplication.class)
+@WebAppConfiguration
+public class AppstoreJavaApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
